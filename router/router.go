b47d753d7eddd5876033666d8154f2ad238eb196
DUPIN <maximedupin1992@gmail.com>
2023-07-29 21:48:35 +0200

    first commit

---
+++ router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"myapi/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router{
+	router:=mux.NewRouter()
+
+	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/getAllStock", middleware.GetAllStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newStock", middleware.CreateStock).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/deleteStock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/updateStock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+}
